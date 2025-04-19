33ed60a4df181c896c7676c61cd4947c86e8f141
xushun <xsshun@gmail.com>
2025-04-19 11:38:46 +0800

    Initial Commit

---
+++ iRelax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iRelax
+//
+//  Created by zoe on 2025/4/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iRelax/iRelaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRelaxApp.swift
+//  iRelax
+//
+//  Created by zoe on 2025/4/19.
+//
+
+import SwiftUI
+
+@main
+struct iRelaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
